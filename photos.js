a4c19cc309f0700de701c56e75ac0c51c8c5d0ca
danya <dreginkogan@gmail.com>
2026-08-25 15:14:11 -0400

    fleshed out the skeleton of the portfolio -- need to add content

---
+++ photos.js
@@ -0,0 +1,56 @@
+// TODO: add more photos and write descriptions
+
+const photoPool = [
+    { 
+        src: "images/abandoned_bridge.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/blurry_campus_sunset.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/brighton_beach.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/coal_barge.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/continental_divide.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/fence.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/fire.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/flowers.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/london_train.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/snowy_campus.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/subway.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/tractor.jpg", 
+        description: "Description..."
+    },
+    { 
+        src: "images/water_tower.jpg", 
+        description: "Description..."
+    }
+];
