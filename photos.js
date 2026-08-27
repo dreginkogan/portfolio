5798c7eaf05696a6e299ba6eca32112c35d351c2
danya <dreginkogan@gmail.com>
2026-08-27 18:55:54 -0400

    added project content, need to look over text, finish adding photography photo descriptions, and add a good headshot

--- photos.js
+++ photos.js
@@ -3,35 +3,35 @@
 const photoPool = [
     { 
         src: "images/abandoned_bridge.jpg", 
-        description: "Description..."
+        description: "Pittsburgh is scattered with old monuments from the high days of the steel industry. This bridge, which once carried red-hot slag across the Monongahela is only one example."
     },
     { 
         src: "images/blurry_campus_sunset.jpg", 
-        description: "Description..."
+        description: "The twilight, after sunset, at Carnegie Mellon is the most beautiful time of day."
     },
     { 
         src: "images/brighton_beach.jpg", 
-        description: "Description..."
+        description: "Brighton Beach, Brooklyn is the first place my relatives lived in when they moved to the US from Ukraine four or so decades ago."
     },
     { 
         src: "images/coal_barge.jpg", 
-        description: "Description..."
+        description: "This barge pushing coal on the Monongahela makes me think of 'Where the Old Allegheny and Monongahela Flow' -- a folk song about Pittsburgh which has been recorded by Pete Seeger."
     },
     { 
         src: "images/continental_divide.jpg", 
-        description: "Description..."
+        description: "The view from The Eastern Continental Divide, seperating the Gulf of Mexico and Cheseapeake Bay watersheds, seen from the Greater Allegheny Passage bike trail (Connecting Pittsburgh and Cumberland) in August."
     },
     { 
         src: "images/fence.jpg", 
-        description: "Description..."
+        description: "My favorite aspect of this photo is how the blue fence and the alley each occupy nearly exactly half of the photo."
     },
     { 
         src: "images/fire.jpg", 
-        description: "Description..."
+        description: "Film photography in the dark is very difficult, but this campfire I made with my friends in the summer of 2025 came out really well."
     },
     { 
         src: "images/flowers.jpg", 
-        description: "Description..."
+        description: "One of my favorite parts of film photography is how light comes out blurry and bright sometimes, which can have a very cool effect."
     },
     { 
         src: "images/london_train.jpg", 
@@ -51,6 +51,6 @@ const photoPool = [
     },
     { 
         src: "images/water_tower.jpg", 
-        description: "Description..."
+        description: "Simple, mundane, parts of urban environments such as water towers, alley ways, staircases, really intrigue me and are some of my favorite things to photograph."
     }
 ];
